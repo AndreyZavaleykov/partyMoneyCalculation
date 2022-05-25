9f9d8428c31b08e7886c8ba085aa3563569fdf84
andrey.zavaleykov <apacer631@gmail.com>
2022-05-25 09:55:24 +0300

    fix

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 const PRODUCTS = [];
-const PEOPLES = [];
+let PEOPLES = [];
+let productsPeople = [];
 
 document.forms.publish.onsubmit = function () {
     const product = {
@@ -11,47 +12,47 @@ document.forms.publish.onsubmit = function () {
     return false;
 };
 document.forms.people.onsubmit = function () {
-    const people = { name: this.people.value };
+    const people = { name: this.people.value, products: [] };
     PEOPLES.push(people);
     renderTable(PRODUCTS, PEOPLES);
     return false;
 };
 
 function renderTable(PRODUCTS, PEOPLES) {
-    if(PRODUCTS.length>=2&&PEOPLES.length>=2){
+    if (PRODUCTS.length >= 2 && PEOPLES.length >= 2) {
         PEOPLES.forEach((people, indexPeople) =>
             PRODUCTS.forEach((product, indexProduct) => {
-                const checkboxe = document.getElementsByName(`${people.name}_${product.name}`);
-                console.log(checkboxe)
-                checkboxe ? Object.assign(PEOPLES[indexPeople], {products : PRODUCTS[indexProduct]}) : "";
-                
+                let checkboxe = document.querySelector(`.${people.name}_${product.name}:checked`) !== null;
+                console.log(checkboxe);
+                console.log(PEOPLES);
+                checkboxe ? PEOPLES[indexPeople].products: '';
             })
         );
     }
-    document.querySelector("table").innerHTML = `<caption>
+    document.querySelector('table').innerHTML = `<caption>
     Список продуктов
     </caption>
     <th>Продукт/Имя</th>
     <tfoot><th>Итого</th></tfoot>`;
     console.table(PEOPLES, PRODUCTS);
     PRODUCTS.forEach(product => {
-        document.querySelector("body > table > tbody:nth-child(2) > tr").innerHTML += ` 
+        document.querySelector('body > table > tbody:nth-child(2) > tr').innerHTML += ` 
         <th>${product.name}</th>`;
-        document.querySelector("body > table > tfoot > tr").innerHTML += ` 
+        document.querySelector('body > table > tfoot > tr').innerHTML += ` 
         <th>${product.price}</th>`;
     });
     PEOPLES.forEach(people => {
-        document.querySelector("body > table > tbody").innerHTML += ` 
+        document.querySelector('body > table > tbody').innerHTML += ` 
         <tr class="${people.name}"> 
         <td>${people.name}</td>
         </tr>`;
         PRODUCTS.forEach(
             element =>
                 (document.querySelector(`body > table > tbody > .${people.name}`).innerHTML += ` 
-                <td> <input type="checkbox" name="${people.name}_${element.name}"></td>`)
+                <td> <input type="checkbox" class="${people.name}_${element.name}"></td>`)
         );
     });
-    document.querySelector("body > table > tbody:nth-child(2) > tr").innerHTML += ` 
+    document.querySelector('body > table > tbody:nth-child(2) > tr').innerHTML += ` 
     <th>Итого</th>`;
     PEOPLES.map(
         people =>
