00703746ed084ddf79fb3435ac9244f5559657fb
andrey.zavaleykov <apacer631@gmail.com>
2022-06-02 18:20:28 +0300

    fix summ

--- js/main.js
+++ js/main.js
@@ -6,11 +6,13 @@ document.forms.publish.onsubmit = function () {
     const product = {
         name: this.product.value,
         price: this.price.value,
+        count: '0',
     };
     PRODUCTS.push(product);
     renderTable(PRODUCTS, PEOPLES);
     return false;
 };
+
 document.forms.people.onsubmit = function () {
     const people = { name: this.people.value, products: [] };
     PEOPLES.push(people);
@@ -19,22 +21,26 @@ document.forms.people.onsubmit = function () {
 };
 
 function renderTable(PRODUCTS, PEOPLES) {
+    PRODUCTS.forEach(product => {
+        product.count = 0;
+    });
     if (PRODUCTS.length >= 2 && PEOPLES.length >= 2) {
-        PEOPLES.forEach((people, indexPeople) =>
+        PEOPLES.forEach((people, indexPeople) => {
+            PEOPLES[indexPeople].products.length = 0;
             PRODUCTS.forEach(product => {
                 let checkboxe = document.querySelector(`.${people.name}_${product.name}:checked`) !== null;
-                // console.log(checkboxe);
-                // console.log(PEOPLES);
-                checkboxe ? PEOPLES[indexPeople].products.push(product) : '';
-            })
-        );
+                if (checkboxe) {
+                    PEOPLES[indexPeople].products.push(product);
+                    product.count++;
+                }
+            });
+        });
     }
     document.querySelector('table').innerHTML = `<caption>
     Список продуктов
     </caption>
     <th>Продукт/Имя</th>
     <tfoot><th>Итого</th></tfoot>`;
-    // console.table(PEOPLES, PRODUCTS);
     PRODUCTS.forEach(product => {
         document.querySelector('body > table > tbody:nth-child(2) > tr').innerHTML += ` 
         <th>${product.name}</th>`;
@@ -47,7 +53,7 @@ function renderTable(PRODUCTS, PEOPLES) {
         <td>${people.name}</td>
         </tr>`;
         PRODUCTS.forEach(element => {
-            if (people.products.find(item => (element.name = item.name == element.name))) {
+            if (people.products.some(item => item.name == element.name)) {
                 document.querySelector(`body > table > tbody > .${people.name}`).innerHTML += ` 
                 <td> <input type="checkbox" class="${people.name}_${element.name}"checked></td>`;
             } else {
@@ -58,13 +64,12 @@ function renderTable(PRODUCTS, PEOPLES) {
     });
     document.querySelector('body > table > tbody:nth-child(2) > tr').innerHTML += ` 
     <th>Итого</th>`;
-    PEOPLES.map(people => {
+    PEOPLES.forEach(people => {
         let summ = 0;
         if (people.products.length > 0) {
-            summ = people.products.map(item => (summ += +item.price));
+            summ = people.products.reduce((result, item) => result + +item.price / item.count, 0).toFixed(0);
         }
         document.querySelector(`.${people.name}`).innerHTML += ` 
             <td>Сумма для ${people.name} : ${summ}</td>`;
-            summ=0;
     });
 }
